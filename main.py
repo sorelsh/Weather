75bf188293ed7e354e4f51e6e939c6a4038cb1c2
sorelsh <sorelsh@gmail.com>
2025-01-03 17:47:53 +0200

    commit main

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ st.markdown("""
 
 secrets = dotenv_values(".env")
 api_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key={YOUR_API_KEY}"
-st.write(api_url.format(YOUR_API_KEY="API_KEY"))
-response = requests.get(api_url.format(YOUR_API_KEY="API_KEY"))
+st.write(api_url.format(YOUR_API_KEY=st.secrets["API_KEY"]))
+response = requests.get(api_url.format(YOUR_API_KEY=st.secrets["API_KEY"]))
 if response.status_code == 200:
     weather_data = response.json()
-    st.write(api_url.format(YOUR_API_KEY="API_KEY"))
     data = pd.json_normalize(weather_data["days"])
 else:
     st.write("API call failed")
